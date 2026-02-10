fd3b79a1f93fd259835a34bae1a765babc442765
Pranav Pitchala <pitchala.pranav@gmail.com>
2026-02-10 02:03:37 -0800

    Update app.py

--- bot/app.py
+++ bot/app.py
@@ -117,3 +117,4 @@ if __name__ == '__main__':
     application.run_polling()
 
 # https://github.com/python-telegram-bot/python-telegram-bot/wiki/Introduction-to-the-API
+# https://docs.python-telegram-bot.org/en/stable/
